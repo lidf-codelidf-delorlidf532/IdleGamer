e85cf28a8aee14b4837c4e3f6236e10bbe7f1416
ShadowDeveloper <wyatt.darley0515@outlook.com>
2021-06-18 09:30:29 -0500

    testing fix

--- index.js
+++ index.js
@@ -282,6 +282,7 @@ for (var i = 0; i < allUpgrades.length; i++) {
 }
 window.addEventListener("load", updateScore);
 controllerEl.addEventListener("click", onControllerClick)
+detectClick(cursorUpgrade)
     //All Intervals:
 
 
